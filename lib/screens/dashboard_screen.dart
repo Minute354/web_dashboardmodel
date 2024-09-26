02e996d568d534ccad6018d717e66ace1374c31b
sajithsminute <sajithminute@gmail.com>
2024-09-26 12:14:38 +0530

    change in pop's

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -184,9 +184,6 @@ class DashboardPage extends StatelessWidget {
       title: Text(label, style: const TextStyle(color: Colors.white)),
       onTap: () {
         Future.delayed(const Duration(milliseconds: 100), () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Navigating to $label')),
-          );
         });
         onTap();
       },
